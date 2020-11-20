1cff7c4cbb6e756aa0df332d7126817382fc8683
Pugsly99 <rylandadams@yahoo.com>
2020-11-20 12:16:22 -0800

    multi options for other currency

--- src/main.js
+++ src/main.js
@@ -13,14 +13,88 @@ $(document).ready(function() {
     let usd = $('#exchange').val();
     usd;
     let selectedCurrency = $('#exchangedTo').val();
+    if (selectedCurrency === "EUR"){
+      Exchanger.getValue()
+        .then(function(exchangerResponse) {
+          if (exchangerResponse instanceof Error) {
+            throw Error(`Currency API error: ${exchangerResponse.message}`);
+          }
+          const exchanged = exchangerResponse.conversion_rates.EUR;
+          console.log(exchanged);
+        });
+    }  else if (selectedCurrency === "RUB"){
+      Exchanger.getValue()
+        .then(function(exchangerResponse) {
+          if (exchangerResponse instanceof Error) {
+            throw Error(`Currency API error: ${exchangerResponse.message}`);
+          }
+          const exchanged = exchangerResponse.conversion_rates.RUB;
+          console.log(exchanged);
+        });
+    } else if (selectedCurrency === "CAD"){
+      Exchanger.getValue()
+        .then(function(exchangerResponse) {
+          if (exchangerResponse instanceof Error) {
+            throw Error(`Currency API error: ${exchangerResponse.message}`);
+          }
+          const exchanged = exchangerResponse.conversion_rates.CAD;
+          console.log(exchanged);
+        });
+    } else if (selectedCurrency === "AED"){
+      Exchanger.getValue()
+        .then(function(exchangerResponse) {
+          if (exchangerResponse instanceof Error) {
+            throw Error(`Currency API error: ${exchangerResponse.message}`);
+          }
+          const exchanged = exchangerResponse.conversion_rates.AED;
+          console.log(exchanged);
+        });
+    } else if (selectedCurrency === "ARS"){
+      Exchanger.getValue()
+        .then(function(exchangerResponse) {
+          if (exchangerResponse instanceof Error) {
+            throw Error(`Currency API error: ${exchangerResponse.message}`);
+          }
+          const exchanged = exchangerResponse.conversion_rates.ARS;
+          console.log(exchanged);
+        });
+    }  else if (selectedCurrency === "AUD"){
+      Exchanger.getValue()
+        .then(function(exchangerResponse) {
+          if (exchangerResponse instanceof Error) {
+            throw Error(`Currency API error: ${exchangerResponse.message}`);
+          }
+          const exchanged = exchangerResponse.conversion_rates.AUD;
+          console.log(exchanged);
+        });
+    } else if (selectedCurrency === "BGN"){
+      Exchanger.getValue()
+        .then(function(exchangerResponse) {
+          if (exchangerResponse instanceof Error) {
+            throw Error(`Currency API error: ${exchangerResponse.message}`);
+          }
+          const exchanged = exchangerResponse.conversion_rates.BGN;
+          console.log(exchanged);
+        });
+    } else if (selectedCurrency === "BRL"){
+      Exchanger.getValue()
+        .then(function(exchangerResponse) {
+          if (exchangerResponse instanceof Error) {
+            throw Error(`Currency API error: ${exchangerResponse.message}`);
+          }
+          const exchanged = exchangerResponse.conversion_rates.BRL;
+          console.log(exchanged);
+        });
+    } else if (selectedCurrency === "BSD"){
+      Exchanger.getValue()
+        .then(function(exchangerResponse) {
+          if (exchangerResponse instanceof Error) {
+            throw Error(`Currency API error: ${exchangerResponse.message}`);
+          }
+          const exchanged = exchangerResponse.conversion_rates.BSD;
+          console.log(exchanged);
+        });
+    }
     
-    Exchanger.getValue(selectedCurrency)
-      .then(function(exchangerResponse) {
-        if (exchangerResponse instanceof Error) {
-          throw Error(`Currency API error: ${exchangerResponse.message}`);
-        }
-        const exchanged = exchangerResponse.conversion_rates.EUR;
-        console.log(exchanged);
-      });
   });
 });
--- src/services/exchanger.js
+++ src/services/exchanger.js
@@ -1,14 +1,14 @@
 export default class Exchanger {
   static async getValue() {
-    return fetch(`https://v6.exchangerate-api.com/v6//latest/USD`)
-    .then(function(response) {
-      if (!response.ok) {
-        throw Error(response.status);
-      }
-      return response.json();
-    })
-    .catch(function(error) {
-      return Error(error);
-    })
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.status);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return Error(error);
+      });
   }
 }
