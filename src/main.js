af7b160ac2d65aeaff536063a72366b654b54c2e
Pugsly99 <rylandadams@yahoo.com>
2020-11-20 10:37:26 -0800

    input for USD

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Exchanger from './services/exchanger';
 
-$;
+
+
+
+
+$(document).ready(function() {
+  $('#exchangeUsd').click(function() {
+     
+  })
+})
